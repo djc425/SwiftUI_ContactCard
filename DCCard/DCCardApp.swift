3635f38bfb3f76534565139b45a20955a45e75d7
David Chester <david.chester4@gmail.com>
2021-10-05 15:24:43 -0400

    Initial Commit

---
+++ DCCard/DCCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCCardApp.swift
+//  DCCard
+//
+//  Created by David Chester on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct DCCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
